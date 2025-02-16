fd6f4e5be83f2e20ac39158c5dcef6a3fc7dd526
neshadx <neshadimagify@gmail.com>
2025-02-16 09:53:33 +0600

    done

---
+++ switch.js
@@ -0,0 +1,18 @@
+let theme = "blue";
+
+switch (theme) {
+    case "light":
+        console.log("light theme applied");
+        break;
+    case "Dark": {
+            console.log("Dark theme applied");
+            break;
+    }
+    case "blue": {
+            console.log("blue theme applied");
+            break;
+    }
+    default: 
+            console.log("default theme applied");
+    
+}
