2221a2493c8a0c701699241a7529a0b14d5a852a
neshadx <neshadimagify@gmail.com>
2025-02-16 09:43:32 +0600

    first commit

---
+++ comparision.js
@@ -0,0 +1,28 @@
+const price = 6000;
+if (price >= 6000){
+    const discount = price * 10 / 100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}
+else {
+    console.log(payAmount);
+}
+
+const myScore = 50;
+const fScore = 80;
+if (myScore > fScore && fScore < myScore) {
+    console.log('Ghurte jabo dhanmondi')
+}
+else {
+    console.log('Jabo na')
+}   
+
+let burgerPrice = 500;
+if(burgerPrice > 500) {
+    console.log('free coke')
+}
+else{
+    console.log('coke 30tk')
+}
+
+
