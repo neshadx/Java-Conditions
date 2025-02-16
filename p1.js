039568695d12b3c4c6a221336934cb71decbe62f
neshadx <neshadimagify@gmail.com>
2025-02-16 09:46:43 +0600

    done

---
+++ p1.js
@@ -0,0 +1,12 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+let burger = 700;
+if (burger > 500) {
+    console.log('free coke');
+} else {
+    console.log('coke 30tk')
+}
